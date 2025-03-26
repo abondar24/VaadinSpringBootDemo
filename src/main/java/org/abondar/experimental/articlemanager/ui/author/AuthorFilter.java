b99d82d717f21120e9267ae872a38f808465ae01
Alex Bondar <abondar1992@gmail.com>
2025-03-26 21:26:28 +0100

    added filter

---
+++ src/main/java/org/abondar/experimental/articlemanager/ui/author/AuthorFilter.java
@@ -0,0 +1,12 @@
+package org.abondar.experimental.articlemanager.ui.author;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class AuthorFilter {
+
+    private String searchTerm;
+
+}
